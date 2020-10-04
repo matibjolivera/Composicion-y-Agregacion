4ce6de455cef8cbf0881357fe73234202ec753ac
Matias Olivera <matibjolivera@gmail.com>
2020-10-03 23:10:07 -0300

    test exercise 2

---
+++ src/Ejercicio2/Test.java
@@ -0,0 +1,38 @@
+package Ejercicio2;
+
+import Ejercicio2.Email;
+import Ejercicio2.LineType;
+import Ejercicio2.Person;
+import Ejercicio2.PhoneNumber;
+
+public class Test {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            int characteristic = 10 + i;
+            int suscriberNumber = 12345678 + i;
+            int countryCode = 54 + i;
+
+            Person person = new Person("Matías", "Olivera");
+
+            Email email1 = new Email("matibjolivera@gmail.com");
+            Email email2 = new Email("matiasolivera@gmail.com");
+
+            PhoneNumber phoneNumber1 = new PhoneNumber(++characteristic, ++suscriberNumber, ++countryCode, LineType.CELULAR);
+            PhoneNumber phoneNumber2 = new PhoneNumber(++characteristic, ++suscriberNumber, ++countryCode, LineType.FIJO);
+
+            Pet pet1 = new Pet("Chino", "Perro");
+            Pet pet2 = new Pet("Teo", "Gato");
+
+            person.addEmail(email1);
+            person.addPhoneNumber(phoneNumber1);
+            person.addPet(pet1);
+
+            person.addEmail(email2);
+            person.addPhoneNumber(phoneNumber2);
+            person.addPet(pet2);
+
+            person.showAll();
+        }
+    }
+}
